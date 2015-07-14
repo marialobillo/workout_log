dbe9c3bba568678877e6c239031b2dede28d1545
Neniah <maria.lobillo.santos@gmail.com>
2015-07-14 18:26:50 +0200

    workouts controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,7 +1,39 @@
 class WorkoutsController < ApplicationController
 
-  def index 
-
+  def index
 
   end
+
+  def show
+
+  end
+
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+    
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+  end
+
+  private
+
+    def workout_params
+
+    end
+
+    def find_workout
+
+    end
 end
