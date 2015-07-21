2863cf2fe7be2bc4ebbf2c68e7d61ebea8adf127
Neniah <maria.lobillo.santos@gmail.com>
2015-07-21 13:23:53 +0200

    final commit

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,10 +1,8 @@
 class ExercisesController < ApplicationController
+	def create
+		@workout = Workout.find(params[:workout_id])
+		@exercise = @workout.exercises.create(params[:exercise].permit(:name, :sets, :reps))
 
-  def create
-    @workout = Workout.find(params[:workout_id])
-    @exercise = @workout.exercise.create(params[:workout].permit(:name, :sets, :reps))
-
-    redirect_to workout_path(@workout)
-  end
-
+		redirect_to workout_path(@workout)
+	end
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,4 +1,4 @@
 class Workout < ActiveRecord::Base
 
-  has_many :exercises
+  has_many :exercises, dependent: :destroy
 end
